d706028befdd941763426ed2fe41a165f4f90879
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-01-20 16:32:53 +0200

    chore: use shorter package naming

---
+++ src/test/java/org/ujar/bs/reactor/acmedepartments/ReactiveDemoAcmeDepartmentsApplicationTest.java
@@ -0,0 +1,18 @@
+package org.ujar.bs.reactor.acmedepartments;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveDemoAcmeDepartmentsApplicationTest {
+
+  @Test
+  void contextLoad() {
+    Assertions.assertDoesNotThrow(this::doNotThrowException);
+  }
+
+  private void doNotThrowException() {
+    // This method will never throw exception
+  }
+}
