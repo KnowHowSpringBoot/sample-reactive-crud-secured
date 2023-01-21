09f4cc417797e03c5c61e2c82d26eb3de6d6516c
Dima Denysenko <coding@dimdnk.com>
2023-01-21 11:15:03 +0200

    chore: use shorter package naming

---
+++ src/main/java/org/ujar/bs/nb/reactor/acmedepartments/ReactorDemoAcmeDepartmentsApplication.java
@@ -0,0 +1,11 @@
+package org.ujar.bs.nb.reactor.acmedepartments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorDemoAcmeDepartmentsApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ReactorDemoAcmeDepartmentsApplication.class, args);
+  }
+}
