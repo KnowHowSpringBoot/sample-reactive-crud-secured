e373438e923e8fecb10f4e717624de7a5adc3044
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2023-01-21 11:15:03 +0200

    chore: use shorter package naming

--- src/main/java/org/ujar/bs/reactor/acmedepartments/entity/UserRole.java
+++
@@ -1,5 +0,0 @@
-package org.ujar.bs.reactor.acmedepartments.entity;
-
-public enum UserRole {
-  ROLE_USER, ROLE_ADMIN
-}
--- src/main/java/org/ujar/bs/reactor/acmedepartments/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.ujar.bs.reactor.acmedepartments.repository;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-import org.ujar.bs.reactor.acmedepartments.entity.Department;
-import reactor.core.publisher.Mono;
-
-public interface DepartmentRepository extends ReactiveCrudRepository<Department, Integer> {
-  Mono<Department> findByUserId(Integer userId);
-}
